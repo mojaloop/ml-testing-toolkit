c44f0ba2591fe10e99b16176114513a963e4db02
Vijay <vijay_g10@yahoo.com>
2019-12-31 02:07:45 +0530

    Added callback map to openapi endpoint in api

--- src/lib/api-routes/openapi.js
+++ src/lib/api-routes/openapi.js
@@ -23,4 +23,20 @@ router.get('/definition/:version', async (req, res, next) => {
   }
 })
 
+router.get('/callback_map/:version', async (req, res, next) => {
+  try {
+    const reqVersion = req.params.version
+
+    try {
+      const reqCallbackMap = require('../../../spec_files/fspiop_versions/' + reqVersion + '/callback_map.json')
+      res.status(200).json(reqCallbackMap)
+    } catch (err) {
+      res.status(404).json({ error: 'Unknown Version' })
+    }
+
+  } catch (err) {
+    next(err)
+  }
+})
+
 module.exports = router
