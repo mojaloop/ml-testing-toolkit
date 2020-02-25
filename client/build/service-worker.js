a87f22274743b1e620b41f1a2f78181ceead8c37
Vijay <vijay_g10@yahoo.com>
2020-02-26 00:21:16 +0530

    Updated client

--- client/build/service-worker.js
+++ client/build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/ui/precache-manifest.1450b999db73355852397dcb7b157dca.js"
+  "/ui/precache-manifest.02dd72e57bac4de9c02fbd6e3e2ca161.js"
 );
 
 workbox.clientsClaim();
