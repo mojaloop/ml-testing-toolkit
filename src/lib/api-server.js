bdb8ebde5ed091848d7e7d1cc771a437c3ffee62
Vijay <vijay_g10@yahoo.com>
2020-03-03 00:48:18 +0530

    refactored express server startup

---
+++ src/lib/api-server.js
@@ -0,0 +1,50 @@
+const path = require('path')
+const express = require('express')
+const app = express()
+const http = require('http').Server(app)
+const socketIO = require('socket.io')(http)
+
+// For CORS policy
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', '*')
+  res.setHeader(
+    'Access-Control-Allow-Headers',
+    'Origin, X-Requested-With, Content-Type, Accept, Authorization'
+  )
+  res.setHeader(
+    'Access-Control-Allow-Methods',
+    'GET, POST, PATCH, PUT, DELETE, OPTIONS'
+  )
+  next()
+})
+
+// For parsing incoming JSON requests
+app.use(express.json())
+
+// For admin API
+app.use('/api/rules', require('./api-routes/rules'))
+app.use('/api/openapi', require('./api-routes/openapi'))
+app.use('/api/outbound', require('./api-routes/outbound'))
+app.use('/api/config', require('./api-routes/config'))
+
+// For front-end UI
+app.use('/ui', express.static(path.join('client/build')))
+
+app.get('*', (req, res) => {
+  res.sendFile(process.cwd() + '/client/build/index.html')
+})
+
+const startServer = port => {
+  http.listen(port)
+  console.log('API Server started on port ' + port)
+}
+
+const getApp = () => {
+  return app
+}
+
+module.exports = {
+  startServer,
+  socketIO,
+  getApp
+}
