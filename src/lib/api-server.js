a46fbbe7d6c46023cbcf9233c9d61b5c878f8a67
Vijay <vijay_g10@yahoo.com>
2020-03-04 14:51:44 +0530

    Fixed some issue regarding init of express app

--- src/lib/api-server.js
+++ src/lib/api-server.js
@@ -4,42 +4,48 @@ const app = express()
 const http = require('http').Server(app)
 const socketIO = require('socket.io')(http)
 
-// For CORS policy
-app.use((req, res, next) => {
-  res.setHeader('Access-Control-Allow-Origin', '*')
-  res.setHeader(
-    'Access-Control-Allow-Headers',
-    'Origin, X-Requested-With, Content-Type, Accept, Authorization'
-  )
-  res.setHeader(
-    'Access-Control-Allow-Methods',
-    'GET, POST, PATCH, PUT, DELETE, OPTIONS'
-  )
-  next()
-})
+const initServer = () => {
+  // For CORS policy
+  app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*')
+    res.setHeader(
+      'Access-Control-Allow-Headers',
+      'Origin, X-Requested-With, Content-Type, Accept, Authorization'
+    )
+    res.setHeader(
+      'Access-Control-Allow-Methods',
+      'GET, POST, PATCH, PUT, DELETE, OPTIONS'
+    )
+    next()
+  })
 
-// For parsing incoming JSON requests
-app.use(express.json())
+  // For parsing incoming JSON requests
+  app.use(express.json())
 
-// For admin API
-app.use('/api/rules', require('./api-routes/rules'))
-app.use('/api/openapi', require('./api-routes/openapi'))
-app.use('/api/outbound', require('./api-routes/outbound'))
-app.use('/api/config', require('./api-routes/config'))
+  // For admin API
+  app.use('/api/rules', require('./api-routes/rules'))
+  app.use('/api/openapi', require('./api-routes/openapi'))
+  app.use('/api/outbound', require('./api-routes/outbound'))
+  app.use('/api/config', require('./api-routes/config'))
 
-// For front-end UI
-app.use('/ui', express.static(path.join('client/build')))
+  // For front-end UI
+  app.use('/ui', express.static(path.join('client/build')))
 
-app.get('*', (req, res) => {
-  res.sendFile(process.cwd() + '/client/build/index.html')
-})
+  app.get('*', (req, res) => {
+    res.sendFile(process.cwd() + '/client/build/index.html')
+  })
+}
 
 const startServer = port => {
+  initServer()
   http.listen(port)
   console.log('API Server started on port ' + port)
 }
 
 const getApp = () => {
+  if (!Object.prototype.hasOwnProperty.call(app, '_router')) { // To check whether app is initialized or not
+    initServer()
+  }
   return app
 }
 
