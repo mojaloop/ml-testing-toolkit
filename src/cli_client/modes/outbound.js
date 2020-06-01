dbcaad72c767bbfd78f5a34479b95e92c15e8a88
vijayg10 <33152110+vijayg10@users.noreply.github.com>
2020-06-01 18:32:51 +0530

    Update outbound.js

--- src/cli_client/modes/outbound.js
+++ src/cli_client/modes/outbound.js
@@ -65,10 +65,10 @@ const sendTemplate = async (configuration) => {
       let index = 1
       collections.forEach(collection => {
         collection.test_cases.forEach(testCase => {
+         const { id, ...remainingTestCaseProps } = testCase
           template.test_cases.push({
             id: index++,
-            name: testCase.name,
-            requests: testCase.requests
+            ...remainingTestCaseProps
           })
         })
       })
