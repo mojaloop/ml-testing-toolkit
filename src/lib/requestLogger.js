8f9dfe54e78c1963ce89b3c5b10fc80fcbb80c0c
Vijay Kumar <vijaya.guthi@modusbox.com>
2021-03-18 23:53:56 +0530

    fixed some unit tests

--- src/lib/requestLogger.js
+++ src/lib/requestLogger.js
@@ -28,6 +28,8 @@
 // const Util = require('util')
 const Logger = require('@mojaloop/central-services-logger')
 const Config = require('./config')
+const NotificationEmitter = require('./notificationEmitter')
+const DBAdapter = require('./db/adapters/dbAdapter')
 const { TraceHeaderUtils } = require('ml-testing-toolkit-shared-lib')
 
 const logRequest = (request, user) => {
@@ -167,16 +169,14 @@ const logMessage = (verbosity, message, externalData = {}) => {
       sessionID = data.request && data.request.customInfo ? data.request.customInfo.sessionID : null
     }
 
-    const notificationEmitter = require('./notificationEmitter')
     if (log.notificationType === 'newOutboundLog') {
-      notificationEmitter.broadcastOutboundLog(log, sessionID)
+      NotificationEmitter.broadcastOutboundLog(log, sessionID)
     } else {
-      notificationEmitter.broadcastLog(log, sessionID)
+      NotificationEmitter.broadcastLog(log, sessionID)
     }
 
     if (hostingEnabled && data.user) {
-      const dbAdapter = require('./db/adapters/dbAdapter')
-      dbAdapter.upsert('logs', log, data.user)
+      DBAdapter.upsert('logs', log, data.user)
     }
   }
 }
