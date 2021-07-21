47c3a66c1de5325b2f53abd9c36162aed86b5900
Vijay Kumar <vijaya.guthi@modusbox.com>
2021-07-21 22:32:51 +0530

    Fixed unit tests

--- test/unit/lib/report-generator/generator.test.js
+++ test/unit/lib/report-generator/generator.test.js
@@ -32,6 +32,100 @@ const sampleJsonReport  = {
 
 }
 
+const properJsonTestTemplate = {
+  name: 'sample',
+  test_cases: [
+    {
+      id: 1,
+      name: 'POST /quotes with wrong headers',
+      meta: {
+        info: 'POST /quotes with wrong headers'
+      },
+      requests: [
+        {
+          request: {
+            id: 1,
+            meta: {
+              info: 'Get quote'
+            },
+            description: 'Get quote',
+            apiVersion: {
+              minorVersion: 0,
+              majorVersion: 1,
+              type: 'fspiop',
+              asynchronous: true
+            },
+            operationPath: '/quotes',
+            method: 'post',
+            headers: {
+              Accept: 'application/vnd.interoperability.quotes+json;version=1.0',
+              'Content-Type': 'application/vnd.interoperability.quotes+json;version=1.0',
+              Date: 'Mon, 11 May 2020 13:39:54 GMT',
+              'FSPIOP-Source': 'testingtoolkitdfsp'
+            },
+            body: {
+              quoteId: '61084f91-d13d-4443-a7ed-a493e0f72eb0',
+              transactionId: '32b5c0de-f02a-4e33-b3f9-5efe9a9cc225',
+              payer: {
+                partyIdInfo: {
+                  partyIdType: 'MSISDN',
+                  partyIdentifier: '44123456789',
+                  fspId: 'testingtoolkitdfsp'
+                },
+                personalInfo: {
+                  complexName: {
+                    firstName: 'Firstname-Test',
+                    lastName: 'Lastname-Test'
+                  },
+                  dateOfBirth: '1984-01-01'
+                }
+              },
+              payee: {
+                partyIdInfo: {
+                  partyIdType: 'MSISDN',
+                  partyIdentifier: '9876543210',
+                  fspId: 'testingtoolkitdfsp'
+                }
+              },
+              amountType: 'SEND',
+              amount: {
+                amount: '100',
+                currency: 'USD'
+              },
+              transactionType: {
+                scenario: 'TRANSFER',
+                initiator: 'PAYER',
+                initiatorType: 'CONSUMER'
+              },
+              note: 'Test Payment'
+            },
+            tests: {
+              assertions: [
+                {
+                  id: 1,
+                  description: 'Response status to be 202',
+                  exec: [
+                    'expect(response.status).to.equal(202)'
+                  ],
+                  resultStatus: {
+                    status: 'SUCCESS'
+                  }
+                }
+              ],
+              passedAssertionsCount: 1
+            },
+            params: {
+              Type: 'MSISDN',
+              ID: '9876543210'
+            },
+            path: '/quotes'
+          }
+        }
+      ]
+    }
+  ]
+}
+
 SpyReadFileAsync.mockResolvedValue(JSON.stringify({}))
 
 
@@ -54,7 +148,7 @@ describe('Report Generator', () => {
         throw new Error('Some error')
       })
       await ReportGenerator.initialize()
-      expect(HandlebarsCompile).toHaveBeenCalledTimes(2);
+      expect(HandlebarsCompile).toHaveBeenCalledTimes(3);
     })
 
     it('generateReport should not throw error for format html', async () => {
@@ -69,7 +163,7 @@ describe('Report Generator', () => {
         // Some functions
       })
       await ReportGenerator.initialize()
-      expect(HandlebarsCompile).toHaveBeenCalledTimes(2);
+      expect(HandlebarsCompile).toHaveBeenCalledTimes(3);
     })
 
     it('generateReport should not throw error for format html', async () => {
@@ -80,6 +174,20 @@ describe('Report Generator', () => {
     })
     it('generateReport should not throw error for format printhtml', async () => {
       await expect(ReportGenerator.generateReport(sampleJsonReport, 'printhtml')).resolves.toBeUndefined()
+    })
+  })
+})
+
+describe('Testcase Definition Generator', () => {
+  describe('Happy Path', () => {
+    it('generateTestcaseDefinition should not throw error for format html', async () => {
+      await expect(ReportGenerator.generateTestcaseDefinition(sampleJsonReport, 'html')).resolves.toBeUndefined()
+    })
+    it('generateTestcaseDefinition should not throw error for format pdf', async () => {
+      await expect(ReportGenerator.generateTestcaseDefinition(sampleJsonReport, 'pdf')).resolves.toBeUndefined()
+    })
+    it('generateTestcaseDefinition should not throw error for format printhtml', async () => {
+      await expect(ReportGenerator.generateTestcaseDefinition(sampleJsonReport, 'printhtml')).resolves.toBeUndefined()
     })
   })
 })
