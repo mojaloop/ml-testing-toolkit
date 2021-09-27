2c9642cc3da23856fde107d2f830a18c444e277e
Vijay Kumar <vijaya.guthi@modusbox.com>
2021-09-28 00:50:12 +0530

    Fixed an issue with keycloak users list

--- src/lib/db/dfspMockUsers.js
+++ src/lib/db/dfspMockUsers.js
@@ -1,7 +1,5 @@
 const Config = require('../config')
-const querystring = require('querystring')
-const axios = require('axios').default
-const objectStore = require('../objectStore/objectStoreInterface')
+const { keycloakAuth, getKeyCloakUsers } = require('../oauth/KeycloakHelper')
 
 const tempDfspList = [
   {
@@ -51,55 +49,6 @@ const getDFSPList = async (defaultTestingToolkitFspId) => {
   return users
 }
 
-const getKeyCloakUsers = async (keycloakToken) => {
-  const systemConfig = Config.getSystemConfig()
-  const getUsersResp = await axios.get(systemConfig.KEYCLOAK.API_URL + `/auth/admin/realms/${systemConfig.KEYCLOAK.REALM}/users`, { headers: { Authorization: `Bearer ${keycloakToken.accessToken}` } })
-  if (getUsersResp.status === 200) {
-    const users = []
-    getUsersResp.data.map(user => {
-      if (user.username !== systemConfig.KEYCLOAK.USERNAME) {
-        let userId = user.attributes.dfspId[0]
-        if (Array.isArray(user.attributes.dfspId)) {
-          userId = user.attributes.dfspId[0]
-        }
-        users.push({
-          id: userId,
-          name: `${user.firstName} ${user.lastName}`
-        })
-      }
-    })
-    return users
-  } else {
-    throw new Error(`Some error while getting users from as ${systemConfig.KEYCLOAK.USERNAME}`)
-  }
-}
-
-const keycloakAuth = async () => {
-  let keycloakToken = await objectStore.get('KEYCLOAK_TOKEN')
-  // if token not set or expires in 1 min, then generate a new one
-  if (Object.keys(keycloakToken).length === 0 || Date.now() >= (keycloakToken.expiresAt - (60 * 1000))) {
-    const systemConfig = Config.getSystemConfig()
-    const loginFormData = {
-      grant_type: 'password',
-      client_id: systemConfig.OAUTH.APP_OAUTH_CLIENT_KEY,
-      client_secret: systemConfig.OAUTH.APP_OAUTH_CLIENT_SECRET,
-      username: systemConfig.KEYCLOAK.USERNAME,
-      password: systemConfig.KEYCLOAK.PASSWORD
-    }
-    const loginResp = await axios.post(systemConfig.OAUTH.OAUTH2_ISSUER, querystring.stringify(loginFormData), { headers: { 'Content-Type': 'application/x-www-form-urlencoded' } })
-    if (loginResp.status === 200) {
-      await objectStore.set('KEYCLOAK_TOKEN', {
-        accessToken: loginResp.data.access_token,
-        expiresAt: Date.now() + (loginResp.data.expires_in * 1000)
-      })
-      keycloakToken = await objectStore.get('KEYCLOAK_TOKEN')
-    } else {
-      throw new Error(`Some error while login to the Keycloak as ${systemConfig.KEYCLOAK.USERNAME}`)
-    }
-  }
-  return keycloakToken
-}
-
 const checkDFSP = async (dfspId) => {
   const dfspFound = (await getDFSPList()).find(item => item.id === dfspId)
   if (dfspFound) {
@@ -111,7 +60,5 @@ const checkDFSP = async (dfspId) => {
 
 module.exports = {
   getDFSPList,
-  checkDFSP,
-  getKeyCloakUsers,
-  keycloakAuth
+  checkDFSP
 }
