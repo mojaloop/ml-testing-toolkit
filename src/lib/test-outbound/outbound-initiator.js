a621133c6437b1ed6185c61f4f6723343470715d
Kevin Leyow <kleyow@gmail.com>
2024-11-04 15:45:38 -0500

    chore: lint

--- src/lib/test-outbound/outbound-initiator.js
+++ src/lib/test-outbound/outbound-initiator.js
@@ -302,7 +302,6 @@ const processTestCase = async (testCase, traceID, inputValues, variableData, dfs
     // Send http request
     let status
     try {
-
       // Extra step to access request variables that consists of environment variables in scripts
       const tmpRequest = replaceEnvironmentVariables(convertedRequest, variableData.environment)
       if (globalConfig.scriptExecution) {
